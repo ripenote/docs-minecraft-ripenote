f674c875964e53577e3af56725326c43f66282ca
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-05-08 02:34:24 +0900

    Update generate.py

--- .og-generate/generate.py
+++ .og-generate/generate.py
@@ -14,8 +14,8 @@ def add_centered_text(base_img, text, font_path, font_size, font_color, height):
     draw = ImageDraw.Draw(base_img)
     
     # 文字がベース画像からはみ出ないように処理
-    if draw.textsize(text, font=font)[0] > base_img.size[0] - 10:
-        while draw.textsize(text + '…', font=font)[0] > base_img.size[0] - 10:
+    if draw.textsize(text, font=font)[0] > base_img.size[0] - 170:
+        while draw.textsize(text + '…', font=font)[0] > base_img.size[0] - 170:
             text = text[:-1]
         text = text + '…'
 
@@ -25,8 +25,8 @@ def add_centered_text(base_img, text, font_path, font_size, font_color, height):
 
 if __name__ == '__main__':
     base_img = Image.open(ogp_base_img_path).copy()
-    base_img = add_centered_text(base_img, '/execute コマンド', font_black_path, 64, (64, 64, 64), 380)
-    base_img = add_centered_text(base_img, 'Created by tamagoez', font_medium_path, 32, (120, 120, 120), 560)
+    base_img = add_centered_text(base_img, '/execute コマンド', font_black_path, 100, (64, 64, 64), 380)
+    base_img = add_centered_text(base_img, 'Created by tamagoez', font_medium_path, 50, (120, 120, 120), 560)
 
     base_img.show()
     print(CONST_REQUEST_PATH)
