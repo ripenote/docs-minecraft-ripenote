7c1f41f19869c9a1546dae7889adff81a4d31f93
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-05-08 00:37:25 +0900

    Create generate.py

---
+++ .og-generate/generate.py
@@ -0,0 +1,29 @@
+# https://zenn.dev/makiart/articles/78d53694e70105
+
+from PIL import Image, ImageFont
+
+ogp_base_img_path = 'base.png'
+font_black_path = "black font path"
+font_medium_path = "medium font path"
+
+def add_centered_text(base_img, text, font_path, font_size, font_color, height):
+    font = ImageFont.truetype(font_path, font_size)
+    draw = ImageDraw.Draw(base_img)
+    
+    # 文字がベース画像からはみ出ないように処理
+    if draw.textsize(text, font=font)[0] > base_img.size[0] - 170:
+        while draw.textsize(text + '…', font=font)[0] > base_img.size[0] - 170:
+            text = text[:-1]
+        text = text + '…'
+
+    draw.text(((base_img.size[0] - draw.textsize(text, font=font)[0]) / 2, height), text, font_color, font=font)
+
+    return base_img
+
+if __name__ == '__main__':
+    base_img = Image.open(ogp_base_img_path).copy()
+    base_img = add_centered_text(base_img, '/execute コマンド', font_black_path, 64, (64, 64, 64), 380)
+    base_img = add_centered_text(base_img, 'Created by tamagoez', font_medium_path, 32, (120, 120, 120), 560)
+
+    base_img.show()
+    base_img.save('output.png')
